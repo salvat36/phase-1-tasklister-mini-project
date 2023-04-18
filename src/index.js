e14bb32769711eff1e0aa941437b0d198685478c
Ryan <salvat36@gmail.com>
2023-04-18 10:44:50 -0500

    Passing Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+ addToDo(e.target.newtaskdescription.value);
+ e.target.newtaskdescription.value.reset()
+})
+})
+
+function addToDo(task){
+  const li = document.createElement("li");
+  li.className = "task-item"
+  li.innerText = task;
+  document.querySelector("#list").appendChild(li);
+
+  const button = document.createElement("button");
+  button.className = "delete-btn"
+  button.innerText = "X";
+  li.appendChild(button);
+  button.addEventListener("click", e => e.target.parentElement.remove())
+}
+
